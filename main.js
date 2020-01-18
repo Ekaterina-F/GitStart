2f42647e798af5e25f5928d1d62605a7b08a7ae1
ovchinnikov.dk <danil42worker@gmail.com>
2020-01-18 19:47:12 +0700

    Add base logic

---
+++ main.js
@@ -0,0 +1,13 @@
+function calc(type, arg1, arg2) {
+    let result = 0;
+    switch (type) {
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+        default:
+            result = 0;
+    }
+
+    return result;
+}
